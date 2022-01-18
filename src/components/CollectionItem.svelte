c811a4c0d7d802df55c86568325117ad1822da87
vxncetxn <thevancetan@gmail.com>
2022-01-19 03:36:44 +0800

    Added page transition for Sun; Componentized CollectionItem;

---
+++ src/components/CollectionItem.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let collection;
+
+	function handleMouseEnter() {
+		let suns = [...document.querySelectorAll(".sun")]
+        suns.forEach((sun, i) => {
+            sun.style.background = `radial-gradient(ellipse at top, ${collection.colors[i]}, transparent)`
+        })
+	}
+</script>
+
+<a href={`/${collection.slug}`} class="mt-10 text-6xl text-transparent cursor-pointer text-stroke hover:text-white">{collection.name}</a>
