ddda435bad00216402b247dde2d1dd963d9d1daa
vxncetxn <thevancetan@gmail.com>
2022-01-19 15:57:53 +0800

    Added color transitions to Sun on CollectionItem hover

--- src/components/CollectionItem.svelte
+++ src/components/CollectionItem.svelte
@@ -2,11 +2,17 @@
     export let collection;
 
 	function handleMouseEnter() {
-		let suns = [...document.querySelectorAll(".sun")]
-        suns.forEach((sun, i) => {
-            sun.style.background = `radial-gradient(ellipse at top, ${collection.colors[i]}, transparent)`
-        })
+		let sunOneAnimate = document.getElementById('sun-one-animate');
+        let sunTwoAnimate = document.getElementById('sun-two-animate');
+
+        sunOneAnimate.setAttribute('from', sunOneAnimate.getAttribute('to'));
+        sunOneAnimate.setAttribute('to', collection.colors[0]);
+        sunTwoAnimate.setAttribute('from', sunTwoAnimate.getAttribute('to'));
+        sunTwoAnimate.setAttribute('to', collection.colors[1]);
+
+        sunOneAnimate.beginElement();
+        sunTwoAnimate.beginElement();
 	}
 </script>
 
-<a href={`/${collection.slug}`} class="mt-10 text-6xl text-transparent cursor-pointer text-stroke hover:text-white">{collection.name}</a>
+<a on:mouseenter={handleMouseEnter} href={`/${collection.slug}`} class="mt-10 text-6xl text-transparent cursor-pointer text-stroke hover:text-white">{collection.name}</a>
